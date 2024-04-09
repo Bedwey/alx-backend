d8fba7846abb82b718092b80515aeaa905f5fc51
Abdullah Bedwey <bedweyy@gmail.com>
2024-04-09 17:36:36 +0000

    fix code and add permisson

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -5,7 +7,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('0-index.html')
 
 
 if __name__ == '__main__':
